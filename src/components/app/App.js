4ad230f576dfd6b19b86b72c79590dee17dda64b
k24dizzle <zhaok24@uw.edu>
2018-10-04 23:01:45 -0400

    Jank

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Board from '../board/board.js';
 
 class App extends Component {
   render() {
+  	console.log("Board");
+  	console.log(Board);
     return (
       <div className="App">
         <h1> Five in a row! </h1>
+        <Board/>
       </div>
     );
   }
---
+++ src/components/board/board.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './board.css';
+
+class Board extends Component {
+  render() {
+    return (
+      <div className="Board">
+        <h1> Blip blip blip </h1>
+      </div>
+    );
+  }
+}
+
+export default Board;
