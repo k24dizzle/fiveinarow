744eafe52b7642275104c37fd8a6a35864066853
k24dizzle <zhaok24@uw.edu>
2018-10-15 23:25:06 -0400

    Figure out how to fill array

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -3,8 +3,8 @@ import './board.css';
 
 class Board extends Component {
     generateTable() {
-      var height = [0, 1, 2, 3, 4, 5];
-      var width = [0, 1, 2, 3, 4, 5];
+      var height = Array.from(Array(parseInt(this.props.height)),(x,i)=>i);
+      var width = Array.from(Array(parseInt(this.props.width)),(x,i)=>i);
       var row = width.map((e, i) => {
         return (<td key={i}>NBA</td>)
       });
@@ -19,26 +19,16 @@ class Board extends Component {
           </tbody>
         </table>
       );
-      // var test = <table>;
-      // for (var i = 0; i < this.props.height; i++) {
-      //  test += "<tr>"
-      //  for (var j = 0; j < this.props.width; j++) {
-      //    test += "<td> <td/>";
-      //  }
-      //  test += "<tr/>"
-      // }
-      // test += "</table>";
-      // return test;
     }
 
     render() {
       var table = this.generateTable();
-      return table;
-        // return (
-        //   <div className="Board">
-        //     <h1> Board </h1>
-        //   </div>
-        // );
+      return (
+        <div className="Board">
+          <h1> Board </h1>
+          { table }
+        </div>
+      );
     }
 }
 
