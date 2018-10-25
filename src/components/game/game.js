4563c8eef14f7c0427a6165ed8d9ff173d27ea6e
k24dizzle <zhaok24@uw.edu>
2018-10-25 00:09:05 -0400

    attempt at win logic

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -20,14 +20,66 @@ class Game extends Component {
       squares: nextSquares,
       xIsNext: !this.state.xIsNext,
       stepNumber: this.state.stepNumber + 1
-
     })
     console.log("[Game] handleClick " + i);
+    this.checkWin(nextSquares.slice(0));
   }
+
+  checkWin(squares) {
+    if (this.checkRows(squares) || this.checkColumns(squares) || this.checkDiagonals(squares)) {
+      alert("Winner");
+    }
+  }
+
+  checkRows(squares) {
+    return this.checkHelper(squares, (i, j) => (i * 15 + j), (i) => (0));
+  }
+  checkColumns(squares) {
+    return this.checkHelper(squares, (i, j) => (j * 15 + i), (i) => (0));
+  }
+  checkDiagonals(squares) {
+    var firstHalf = this.checkHelper(squares, (i, j) => (j * 15 + (j + i)), (i) => (i));
+    squares.reverse();
+    var secondHalf = this.checkHelper(squares, (i, j) => (j * 15 + (j + i)), (i) => (i));
+    return firstHalf || secondHalf;
+  }
+
+  checkHelper(squares, fun, jfun) {
+    // Check if the game is over
+    var combo = 0;
+    var curComboValue = "";
+    var threshold = 5; // 5 in a row to win
+    // Go every diagonal, every row, every column
+    for (var i = 0; i < 15; i++) {
+      for (var j = jfun(i); j < 15; j++) {
+        var index = fun(i, j);
+        if (index < 15 * 15 && squares[index] != null) {
+          if (squares[index] === curComboValue) {
+            combo++;
+          } else {
+            combo = 1;
+            curComboValue = squares[index];
+          }
+          if (combo === threshold) {
+            return true;
+          }
+          console.log(combo + " " + index)
+        } else {
+          combo = 0;
+          curComboValue = "";
+        }
+      }
+      // Reset!
+      combo = 0;
+      curComboValue = "";
+    }
+    return false;
+  }
+
   render() {
     return <Board
               squares={this.state.squares}
-              height="10" width="10"
+              height="15" width="15"
               onClick={i => this.handleClick(i)}
             />;
   }
