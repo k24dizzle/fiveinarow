46dfa59bfed24a8745a9e237aa8c0492ea0ef156
k24dizzle <zhaok24@uw.edu>
2018-11-03 00:57:36 -0400

    Test build

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Five in a row! </h1>
+        <h1> Five In A Row! </h1>
         <div className="boardContainer">
           <Game height="15" width="15"/>
         </div>
